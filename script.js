fe0e3f5be91ccbcdc150b8aef01b489420b017b2
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2025-01-12 16:35:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+function loadImage(url) {
+  return new Promise((resolve, reject) => {
+    let img = new Image();
+    img.src = url;
+    img.onload = () => resolve(img);
+    img.onerror = () => reject(new Error(`Failed to load image's URL: ${url}`));
+  });
+}
